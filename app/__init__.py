27a36d773a3f147790232fb910754defad41db07
brwillian <wiliam-m-@hotmail.com>
2023-03-11 14:02:11 -0300

    :wrench added db access

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = FlaskAPI(__name__)
 app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = ""
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+from app.models import person
 from app.routers import default
